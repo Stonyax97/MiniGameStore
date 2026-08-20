e3b39bf3f490ba84f1e9dc331d5c6e08021d514c
Stonyax97 <zmoukhsil@gmail.com>
2026-08-20 14:17:26 +0100

    Required for v1.4.1 or Later
    Contains the games dictionary.
    >> Required for v1.4.1 or Later

---
+++ games.py
@@ -0,0 +1,79 @@
+games = [
+    {
+        "Name": "Minecraft",
+        "Price": "29.99$",
+        "Genre": "Adventure",
+        "Owned": False,
+        "ID": 0
+    },
+    {
+        "Name" : "Grand Theft Auto V",
+        "Price" : "59.99$",
+        "Genre" : "Action-Adventure",
+        "Owned" : False,
+        "minfo" : "Contains in app-purchases",
+        "ID" : 1
+    },
+    {
+        "Name": "FIFA",
+        "Price": "59.99$",
+        "Genre": "Sports",
+        "Owned": False,
+        "minfo" : "Contains in app-purchases",
+        "ID": 2
+    },
+    {
+        "Name" : "Tetris",
+        "Price" : "1.99$",
+        "Genre" : "Retro",
+        "Owned" : False,
+        "ID" : 3
+    },
+    {
+        "Name" : "Roblox",
+        "Price" : "FREE",
+        "Genre" : "Lobby-Game",
+        "Owned" : False,
+        "minfo" : "Contains in app-purchases",
+        "ID" : 4
+    },
+    {
+        "Name" : "Geometry dash",
+        "Price" : "4.99$",
+        "Genre" : "Action-Platformer",
+        "Owned" : False,
+        "minfo" : "Contains in app-purchases",
+        "ID" : 5
+    },
+    {
+        "Name" : "Among Us",
+        "Price" : "0.99$",
+        "Genre" : "Social-Deduction, Party",
+        "Owned" : False,
+        "minfo" : "Contains in app-purchases",
+        "ID" : 6
+    },
+    {
+        "Name" : "Snake",
+        "Price" : "FREE",
+        "Genre" : "Classic",
+        "Owned" : False,
+        "ID" : 7
+    },
+    {
+        "Name" : "eFootball",
+        "Price" : "FREE",
+        "Genre" : "Sports",
+        "Owned" : False,
+        "minfo" : "Contains in app-purchases",
+        "ID" : 8
+    },
+    {
+        "Name" : "Fortnite",
+        "Price" : "FREE",
+        "Genre" : "3D-Shooter, Battle", 
+        "Owned" : False,
+        "minfo" : "Contains in app-purchases",
+        "ID" : 9
+    }
+]
